b4c31cbe0eb764a647b42dc7210370ff1681538c
Saksham Awasthi <sakshamawasthi1111@gmail.com>
2024-03-14 21:25:19 +0530

    added s3 bucket

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+# Define a simple resource (e.g., an AWS EC2 instance)
+resource "aws_instance" "example" {
+  ami           = "ami-07d9b9ddc6cd8dd30"
+  instance_type = "t2.micro"
+}
+resource "aws_s3_bucket" "terrateam_bucket" {
+  bucket = "terrateam-dev01"
+  acl    = "private"
+}
